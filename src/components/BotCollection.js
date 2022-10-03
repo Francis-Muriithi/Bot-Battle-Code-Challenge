0367142356bb67955e5a252715276d87be909966
Francis Muriithi <mufraggie@gmail.com>
2022-10-03 10:51:32 +0300

    Army added

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -11,8 +11,6 @@ function BotCollection(props) {
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
-        Collection of all bots
-        
         {bots.map((bot) => {
             return <BotCard key={bot.id} bot={bot} onClick={addBot} />;
           })}
